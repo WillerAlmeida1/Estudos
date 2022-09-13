b5c7d09381266477b9c8f29be255cfda42bffe26
Willer Almeida Silva <contatowilleralmeida@gmail.com>
2022-09-13 20:42:44 -0300

    comecando a brincar com o app

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -3,10 +3,12 @@ const express = require("express");
 
 const app = express();
 
-app.get("/primeira-rota", (request, response) => {
-  response.json({
-    message: "acessou a primeira rota"
-  })
-});
+const products = [];
+
+app.post("/products", (request, response) =>{
+  //Nome e preço
+  const body = request.body;
+  console.log(body)
+})
 
 app.listen(4002, () => console.log("Está rodando na porta 4002"))
