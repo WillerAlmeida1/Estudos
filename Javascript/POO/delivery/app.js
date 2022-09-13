a7fe98da6cbd99f833411fab8f1f42fe2756845c
Willer Almeida Silva <contatowilleralmeida@gmail.com>
2022-09-13 13:58:40 -0300

    utilizando imports e exports

--- Javascript/POO/delivery/app.js
+++ Javascript/POO/delivery/app.js
@@ -1,22 +1,7 @@
+import capitalizar from "./modificador"
+import ordenar from "./modificador"
+
 var ingredientes = ['mel', 'água', 'sal', 'mostarda']
-
-function capitalizar(ingredientes) {
-  modificado = []
-  for (var i = 0; i < ingredientes.length; i++) {
-    var letraInicial = ingredientes[i].charAt(0).toUpperCase()
-    var restoTexto = ingredientes[i].slice(1)
-    var resultado = letraInicial + restoTexto
-
-    modificado[i] = resultado
-  }
-  return modificado
-}
-
-function ordenar(ingredientes) {
-  return ingredientes.sort(function (a, b) {
-    return a.localeCompare(b)
-  })
-}
 
 var resultadoCapitalizar = capitalizar(ingredientes)
 var resultadoOrdenar = ordenar(resultadoCapitalizar)
---
+++ Javascript/POO/delivery/modificador.js
@@ -0,0 +1,20 @@
+function capitalizar(vetor) {
+  modificado = []
+  for (var i = 0; i < vetor.length; i++) {
+    var letraInicial = vetor[i].charAt(0).toUpperCase()
+    var restoTexto = vetor[i].slice(1)
+    var resultado = letraInicial + restoTexto
+
+    modificado[i] = resultado
+  }
+  return modificado
+}
+
+function ordenar(vetor) {
+  return vetor.sort(function (a, b) {
+    return a.localeCompare(b)
+  })
+}
+
+//export default {capitalizar, ordenar};
+export default capitalizar; ordenar;
